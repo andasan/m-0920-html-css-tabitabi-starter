c17c4d9890b69e8603b4a4f3038c20237c114c4a
Francois Polo <andasan@gmail.com>
2020-12-14 08:44:11 -0800

    wkd

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // ********** nav toggle ************
 // select button and links
+//CSS is already provided for this, don't have to worry about nav bar :)
 const navBtn = document.getElementById("nav-toggle");
 const links = document.getElementById("nav-links");
 // add event listener
